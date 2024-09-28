a53f02c95ba39b1d037dfbb2861d06668c9a1b05
Timur <rakhmanovtim@gmail.com>
2024-09-28 18:42:16 +0900

    User the provided UserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,3 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-
-class AccountCreateForm(forms.ModelForm):
-
-    class Meta:
-        model = User
-        fields = ("email", "username", "password")
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 from . import forms
 
@@ -8,5 +11,15 @@ from . import forms
 
 class AccountCreate(CreateView):
     model = User
-    form_class = forms.AccountCreateForm
+    form_class = UserCreationForm
     template_name = "account_create.html"
+    success_url = reverse_lazy("accounts:login")
+
+
+class AccountLogin(LoginView):
+    template_name = "account_login.html"
+    success_url = reverse_lazy("accounts:create")
+
+
+class AccountLogout(LogoutView):
+    template_name = "account_logout.html"
