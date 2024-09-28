81523b5664b9d33a0bdaacac887bb7713e5d8cab
Timur <rakhmanovtim@gmail.com>
2024-09-28 18:47:31 +0900

    Add the login redirect url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("create/", views.AccountCreate.as_view(), name="account_create"),
+    path("create/", views.AccountCreate.as_view(), name="create"),
+    path("login/", views.AccountLogin.as_view(), name="login"),
 ]
