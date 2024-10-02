59bbc0c0e4d081e9a4fa98a5a05364f061cd24f5
Timur <rakhmanovtim@gmail.com>
2024-10-02 21:52:39 +0900

    Hide the logic for parsing html

--- recipe_integration/views.py
+++ recipe_integration/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import generic
-from . import forms
+from . import forms, utils
 
-from bs4 import BeautifulSoup
-
-import os
 
 def main(request):
     if request.method == "POST":
@@ -18,24 +15,7 @@ def main(request):
 
 
 def recipe_fetch(request, recipe_id):
-    html_raw = open(os.path.join("./recipe_integration/static/samples/", f"{recipe_id}.html"))
-    html_doc = html_raw.read()
-    html_raw.close()
-    soup = BeautifulSoup(html_doc, "html.parser")
-    materials = soup.find_all(class_="recipe_material__item_name")
-    # materials.find_all(class_="recipe_material__item_name")
-    categories = [material.a for material in materials]
-    # number_of_people per serving 
-    # name of the dish
-    headings_2 = soup.find_all(class_="contents_title")
-    servings = str(headings_2[0].contents).replace("['", "").replace("']", "")
-    ingridients = []
-    for ingr in materials:
-        if ingr is not None:
-            if len(ingr) > 1:
-                # ing_line = ingr[1]
-                ingr = ingr.a
-            ingridients.append(str(ingr.contents).replace("['", "").replace("']", "").replace('★', ""))
+    ingridients, servings = utils.parse_html(recipe_id)
     return render(request, "ingridients.html", context={"ingridients": ingridients, "serving": servings})
 
     
