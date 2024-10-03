ee4f6096a0d0617d96ff12735cedbff5133136e4
Timur <rakhmanovtim@gmail.com>
2024-10-03 17:45:25 +0900

    Add new sample data

--- recipe_integration/views.py
+++ recipe_integration/views.py
@@ -15,12 +15,10 @@ def main(request):
 
 
 def recipe_fetch(request, recipe_id):
-    import time
     name, servings, ingredients, amount = utils.get_ingredients(recipe_id)
     api_data = []
     ingredients = {ingredient: amount for (ingredient, amount) in zip(ingredients, amount)}
     for ingredient, amount in ingredients.items():
-        data = utils.get_result(ingredient, amount, recipe_id)
-        time.sleep(2)
+        data = utils.get_result(item_name=ingredient, item_amount=amount, recipe_id=recipe_id, local_cache=True)
         api_data.extend(data)
     return render(request, "ingredients.html", context={"fetch_data": api_data, "serving": servings, "name": name,})
