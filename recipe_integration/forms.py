49c758faac6cf90be2763037c31b84fa78e0c054
Timur <rakhmanovtim@gmail.com>
2024-10-03 16:22:01 +0900

    Accept only integers in the input field

--- recipe_integration/forms.py
+++ recipe_integration/forms.py
@@ -2,6 +2,4 @@ from django import forms
 
 
 class RecipeFetchForm(forms.Form):
-    recipe_id = forms.IntegerField(widget=forms.TextInput(), required=True)
-
-
+    recipe_id = forms.IntegerField(widget=forms.TextInput(attrs={"required": True, "type": "number", "min": 0,}), required=True, label="Recipe ID")
--- recipe_integration/tests.py
+++ recipe_integration/tests.py
@@ -5,3 +5,6 @@ class Test(TestCase):
     def test_main(self):
         response = self.client.get("http://127.0.0.1:8000/integration/recipe/")
         self.assertEquals(response.status_code, 200)
+
+    def test_input_id(self):
+        response = self.client.post("http://127.0.0.1:8000/integration/recipe/wqeqw")
